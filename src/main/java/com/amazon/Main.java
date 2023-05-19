87ff4bd89cc9ba729ad06e882df917659c715326
dharani <gdharani0598@gail.com>
2023-05-19 17:33:08 -0500

    added comment main

--- src/main/java/com/amazon/Main.java
+++ src/main/java/com/amazon/Main.java
@@ -6,7 +6,7 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello !!");
                 System.out.println("Hello java!!");
-        System.out.println("Hello java!!");
+        System.out.println("Hello java!!"); //hello
 
 
     }
