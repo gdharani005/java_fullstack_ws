94184924d17e8f13aa7c044ea84ba70fa373a07c
gdharani005 <133810652+gdharani005@users.noreply.github.com>
2023-05-24 12:40:23 -0500

    Update helloworld.java

--- src/main/java/com/amazon/util/helloworld.java
+++ src/main/java/com/amazon/util/helloworld.java
@@ -2,6 +2,6 @@ package com.amazon.util;
 
 public class helloworld {
     public static void main(String[] args) {
-        System.out.println("Hi"); //comment added
+        System.out.println("Hi"); //comment added here
     } 
 }
