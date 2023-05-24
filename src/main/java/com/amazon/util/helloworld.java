97311edf92147a2e161bf40901e003b98837bfeb
dharani <gdharani0598@gail.com>
2023-05-24 12:02:46 -0500

    checking push command

--- src/main/java/com/amazon/util/helloworld.java
+++ src/main/java/com/amazon/util/helloworld.java
@@ -2,6 +2,8 @@ package com.amazon.util;
 
 public class helloworld {
     public static void main(String[] args) {
-        System.out.println("Hi"); //comment added
-    } 
+        System.out.println("Hi");
+        //checking push command
+
+    }
 }
